46a53586df361f0751cb18bef749948600052781
karenvillarreal91 <karenvillarreal9150@gmail.com>
2020-07-11 16:39:58 -0300

    Switch123

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,22 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-
-
-
-
+	var mesDelAño = txtIdMes.value;
+		switch (mesDelAño)
+			{
+				case "Julio":
+					case "Agosto":
+						alert ("Abrigate que hace frío.");
+					break;
+				
+				case "Septiembre":
+					case "Octubre":
+						case "Noviembre":
+							alert ("Ya pasamos el frío, ahora el calor.");
+						break;
+				
+				default:
+					alert ("Falta para el invierno.");
+				break;
+			}
 }//FIN DE LA FUNCIÓN
