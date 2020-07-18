3a8aa824821e8d5426bb49a520d630d133b5c167
karenvillarreal91 <karenvillarreal9150@gmail.com>
2020-07-18 03:41:23 -0300

    Switch 9-10

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,87 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacionIngresada;
+	var destinoIngresado;
+	var tarifaTotal;
+	
+		estacionIngresada = txtIdEstacion.value;
+		destinoIngresado = txtIdDestino.value;
 
+		tarifaTotal = parseInt(tarifaTotal);
+
+		if(estacionIngresada == "Invierno")
+		{
+			switch(destinoIngresado)
+				{
+					case "Bariloche":
+
+						tarifaTotal = 15000 * 1.20;
+
+						alert("La tarifa a pagar +20% es de: "+tarifaTotal+".");
+					break;
+
+					case "Cataratas":
+					case "Cordoba":
+
+						tarifaTotal = 15000 - (15000 * 0.10);
+
+						alert("La tarifa a pagar -10% es de: "+tarifaTotal+".");
+					break;
+
+					case "Mar del plata":
+
+						tarifaTotal = 15000 - (15000 * 0.20);
+
+						alert("La tarifa a pagar -20% es de: "+tarifaTotal+".");
+					break;
+				}
+		}
+		if(estacionIngresada == "Verano")
+		{
+			switch(destinoIngresado)
+				{
+					case "Bariloche":
+
+						tarifaTotal = 15000 - (15000 * 0.20);
+					
+						alert("La tarifa a pagar -20% es de: "+tarifaTotal+".");
+					break;
+
+					case "Cataratas":
+					case "Cordoba":
+
+						tarifaTotal = 15000 * 1.10;
+
+						alert("La tarifa a pagar +10% es de: "+tarifaTotal+".");
+					break;
+
+					case "Mar del plata":
+
+						tarifaTotal = 15000 * 1.20;
+
+						alert("La tarifa a pagar +20% es de: "+tarifaTotal+".");
+					break;
+				}
+		}
+		if((estacionIngresada == "Otoño") || (estacionIngresada == "Primavera"))
+		{
+			switch(destinoIngresado)
+				{
+					case "Bariloche":
+					case "Cataratas":
+					case "Mar del plata":
+
+						tarifaTotal = 15000 * 1.10;
+					
+						alert("La tarifa a pagar +10% es de: "+tarifaTotal+".");
+					break;
+
+					case "Cordoba":
+
+						tarifaTotal = 15000;
+
+						alert("La tarifa a pagar es de: "+tarifaTotal+".");
+					break;
+				}
+		}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,58 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacionIngresada;
+	var destinoIngresado; 
+		
+		estacionIngresada = txtIdEstacion.value;
+		destinoIngresado = txtIdDestino.value;
 
+		if(estacionIngresada == "Invierno")
+		{
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+					alert("Se viaja.");
+				break;
+
+				default:
+					alert("No se viaja.");
+				break;
+			}
+		}
+		if(estacionIngresada == "Verano")
+		{
+			switch(destinoIngresado)
+			{
+				case "Mar del plata":
+				case "Cataratas":
+					alert("Se viaja.");
+				break;
+
+				default:
+					alert("No se viaja.");
+				break;
+			}
+		}
+		if(estacionIngresada == "Otoño")
+		{
+			switch(destinoIngresado)
+			{
+				default:
+					alert("Se viaja.");
+				break;
+			}
+		}
+		if(estacionIngresada == "Primavera")
+		{
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+					alert("No se viaja.");
+				break;
+
+				default:
+					alert("Se viaja.");
+				break;
+			}
+		}
 }//FIN DE LA FUNCIÓN
